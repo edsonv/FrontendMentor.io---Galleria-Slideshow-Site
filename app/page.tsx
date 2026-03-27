4f6debe6dd2f4203b044b47fb5d33c1e2405fe68
Edson J. Vargas <edson.jhair@yahoo.com>
2026-03-26 19:33:41 -0500

    Finished home page for all breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { GalleryCard } from "@/components/GalleryCard";
+import data from "@/lib/data.json";
+import { useWindowWidth } from "@/lib/hooks/useWindowWidth";
+import { masonry } from "@/lib/utils/masonryBuilder";
+import { useMemo } from "react";
+
 function HomePage() {
-	return <div>Home Page</div>;
+  const items = data as Painting[];
+  const width = useWindowWidth();
+  const numCols = useMemo(() => {
+    if (!width) return 1;
+    if (width >= 1440) return 4;
+    if (width >= 768) return 2;
+
+    return 1;
+  }, [width]);
+
+  const columns = useMemo(() => masonry({ items, numCols }), [items, numCols]);
+
+  return (
+    <div className="tablet:mx-7 tablet:mb-7 desktop:mx-5 desktop:mb-5 mx-4 mb-4 flex gap-5">
+      {columns.map((column, colIndex) => {
+        return (
+          <div key={colIndex} className="flex flex-1 flex-col gap-5">
+            {column.map((item) => {
+              const originalIndex = items.indexOf(item);
+
+              return (
+                <GalleryCard
+                  item={item}
+                  originalIndex={originalIndex}
+                  key={item.name}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default HomePage;
