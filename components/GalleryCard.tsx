3b5c46fc9a3cba20d3ec252d814cae760988036d
Edson J. Vargas <edson.jhair@yahoo.com>
2026-03-26 19:32:48 -0500

    Add main page gallery card

---
+++ components/GalleryCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const GalleryCard = ({
+  item,
+  originalIndex,
+}: {
+  item: Painting;
+  originalIndex: number;
+}) => {
+  return (
+    <Link
+      href={`/${originalIndex}`}
+      key={item.name}
+      className="card relative hover:mask-[linear-gradient(to_top,rgba(0,0,0,0.9)_16%,rgba(0,0,0,.5)_100%)]"
+    >
+      <picture className="block">
+        <Image
+          src={item.images.thumbnail.src}
+          alt={item.name}
+          width={item.images.thumbnail.width}
+          height={item.images.thumbnail.height}
+          className="h-auto w-full object-contain"
+          priority
+        />
+      </picture>
+      <div className="absolute bottom-0 left-0 z-20 p-4">
+        <div className="text-preset-2-mobile text-white">{item.name}</div>
+        <div className="text-preset-5 mt-1 text-white">{item.artist.name}</div>
+      </div>
+      <div className="absolute top-0 left-0 z-10 h-full w-full bg-linear-[to_top,rgba(0,0,0,.84),rgba(0,0,0,0)]"></div>
+    </Link>
+  );
+};
